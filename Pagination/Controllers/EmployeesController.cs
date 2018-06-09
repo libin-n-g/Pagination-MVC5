a177278d0139af8cf74b6f72badcd4dc425021ba
libin-n-g <libinngeorge@gmail.com>
2018-06-09 22:21:46 +0530

    Adds pagination

---
+++ Pagination/Controllers/EmployeesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Pagination.Models;
+using Pagination.Repository;
+
+namespace Pagination.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private PaginationContext db = new PaginationContext();
+
+        // GET: Employees
+        public async Task<ActionResult> Index(int? page)
+        {
+            int pageNo = 0;
+            pageNo = page == null ? 1 : int.Parse(page.ToString());
+
+            int totalEmployee = db.Employees.Count();
+            int employeePerPage = 20;
+            int inEachPageEmployeeEndAt = pageNo * employeePerPage;
+            int inEachPageEmployeeStartsFrom = inEachPageEmployeeEndAt - employeePerPage;
+            var employees = db.Employees.OrderBy(e => e.EmployeeID).Skip(inEachPageEmployeeStartsFrom).Take(employeePerPage);
+            Pager<Employee> pager = new Pager<Employee>(employees.AsQueryable(), pageNo, employeePerPage, totalEmployee);
+            return View(pager);
+        }
+
+        // GET: Employees/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "EmployeeID,FirstName,LastName,Title,EmailAddress,DepartmentName")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "EmployeeID,FirstName,LastName,Title,EmailAddress,DepartmentName")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = await db.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Employee employee = await db.Employees.FindAsync(id);
+            db.Employees.Remove(employee);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
